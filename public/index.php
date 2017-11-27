d389ee63e7eac0e9733b6c5894d5c0a78e239ac3
赛冷思 <silence@sailengsi.com>
2017-11-28 07:21:28 +0800

    debug4

--- public/index.php
+++ public/index.php
@@ -5,10 +5,14 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 header('Access-Control-Allow-Methods: OPTIONS,GET, POST, PUT, DELETE');
 header("Access-Control-Allow-Credentials:true");
 
-//echo 'test';
-//die;
+
 
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
+
+
+//echo 'test';
+//die;
+
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
