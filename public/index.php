fa33316fdd85ee284a7d42f1ca72d68ce38e6587
赛冷思 <silence@sailengsi.com>
2017-11-28 07:22:27 +0800

    debug4.2

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header('Access-Control-Allow-Methods: OPTIONS,GET, POST, PUT, DELETE');
-header("Access-Control-Allow-Credentials:true");
+//header('Access-Control-Allow-Origin: *');
+//header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+//header('Access-Control-Allow-Methods: OPTIONS,GET, POST, PUT, DELETE');
+//header("Access-Control-Allow-Credentials:true");
 
 
 
@@ -11,8 +11,8 @@ header("Access-Control-Allow-Credentials:true");
 define('APP_PATH', __DIR__ . '/../application/');
 
 
-echo 'test';
-die;
+//echo 'test';
+//die;
 
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
